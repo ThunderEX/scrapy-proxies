bf59c35b17044e86a395daa03342b578c6842911
ThunderEX <thunderex@gmail.com>
2016-09-20 10:02:15 +0800

    Clean up original repo: add gitignore, remove unnecessary package import, remove LICENSE section which already exist in LICENSE.txt, remove unnecessary attribute, add dependency for Scrapy

--- scrapy_proxies/randomproxy.py
+++ scrapy_proxies/randomproxy.py
@@ -1,23 +1,3 @@
-# Copyright (C) 2013 by Aivars Kalvans <aivars.kalvans@gmail.com>
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 import re
 import random
 import base64
@@ -27,12 +7,12 @@ log = logging.getLogger('scrapy.proxies')
 
 class RandomProxy(object):
     def __init__(self, settings):
-        self.proxy_list = settings.get('PROXY_LIST')
+        proxy_list = settings.get('PROXY_LIST')
 
-        if self.proxy_list is None:
+        if proxy_list is None:
             raise KeyError('PROXY_LIST setting is missing')
 
-        fin = open(self.proxy_list)
+        fin = open(proxy_list)
 
         self.proxies = {}
         for line in fin.readlines():
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-import sys
 from distutils.core import setup
 
-setup(name='scrapy-proxies',
-        version='0.3',
-        description='Scrapy Proxies: random proxy middleware for Scrapy',
-        author='Aivars Kalvans',
-        author_email='aivars.kalvans@gmail.com',
-        url='https://github.com/aivarsk/scrapy-proxies',
-        packages=['scrapy_proxies'],
-        )
+setup(
+    name='scrapy-proxies',
+    version='0.3',
+    description='Scrapy Proxies: random proxy middleware for Scrapy',
+    author='Aivars Kalvans',
+    author_email='aivars.kalvans@gmail.com',
+    url='https://github.com/aivarsk/scrapy-proxies',
+    packages=['scrapy_proxies'],
+    install_requires=['Scrapy'],
+)
