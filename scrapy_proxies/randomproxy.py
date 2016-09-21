640b0df08ca6d9f4189e8791d21cf32eeb1fd76a
ThunderEX <thunderex@gmail.com>
2016-09-21 13:37:53 +0800

    refactor to make a composition of built-in HttpProxyMiddleware which improves compatibility with built-in middleware, and also directly move setting from individual file to inside settings.py

--- scrapy_proxies/randomproxy.py
+++ scrapy_proxies/randomproxy.py
@@ -1,66 +1,61 @@
-import re
+import os
 import random
-import base64
 import logging
+from scrapy.exceptions import NotConfigured
+from scrapy.downloadermiddlewares.httpproxy import HttpProxyMiddleware
+from scrapy.utils.httpobj import urlparse_cached
 
-log = logging.getLogger('scrapy.proxies')
+log = logging.getLogger(__name__)
 
 class RandomProxy(object):
-    def __init__(self, settings):
-        proxy_list = settings.get('PROXY_LIST')
 
-        if proxy_list is None:
-            raise KeyError('PROXY_LIST setting is missing')
+    def __init__(self, crawler):
+        auth_encoding = crawler.settings.get('HTTPPROXY_AUTH_ENCODING', 'latin-1')
+        randomproxy_pool = crawler.settings.getdict('RANDOMPROXY_POOL', {})
 
-        fin = open(proxy_list)
+        self.pool = {}
+        backup = os.environ.get('HTTP_PROXY')
+        try:
+            for url in randomproxy_pool:
+                os.environ['HTTP_PROXY'] = url
+                midware = HttpProxyMiddleware(auth_encoding)
+                proxy_type = next(iter(midware.proxies))
+                self.pool.setdefault(proxy_type, [])
+                self.pool[proxy_type].append(midware)
+        finally:
+            if backup:
+                os.environ['HTTP_PROXY'] = backup
 
-        self.proxies = {}
-        for line in fin.readlines():
-            parts = re.match('(\w+://)(\w+:\w+@)?(.+)', line.strip())
-            if not parts:
-                continue
-
-            # Cut trailing @
-            if parts.group(2):
-                user_pass = parts.group(2)[:-1]
-            else:
-                user_pass = ''
-
-            self.proxies[parts.group(1) + parts.group(3)] = user_pass
-
-        fin.close()
+        if not self.pool:
+            raise NotConfigured
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(crawler.settings)
+        return cls(crawler)
 
     def process_request(self, request, spider):
-        # Don't overwrite with a random one (server-side state for IP)
-        if 'proxy' in request.meta:
-            return
+        parsed = urlparse_cached(request)
+        scheme = parsed.scheme
 
-        if len(self.proxies) == 0:
-            raise ValueError('All proxies are unusable, cannot proceed')
+        try:
+            midware = random.choice(self.pool[scheme])
+        except (IndexError, KeyError):
+            raise ValueError('No available proxy, cannot proceed')
 
-        proxy_address = random.choice(list(self.proxies.keys()))
-        proxy_user_pass = self.proxies[proxy_address]
-
-        request.meta['proxy'] = proxy_address
-        if proxy_user_pass:
-            basic_auth = 'Basic ' + base64.encodestring(proxy_user_pass)
-            request.headers['Proxy-Authorization'] = basic_auth
-        log.debug('Using proxy <%s>, %d proxies left' % (
-                    proxy_address, len(self.proxies)))
+        log.debug('Using proxy <%s>, %d %s proxies left' % (
+            midware.proxies[scheme], len(self.pool[scheme]), scheme))
+        midware.process_request(request, spider)
 
     def process_exception(self, request, exception, spider):
         if 'proxy' not in request.meta:
             return
 
-        proxy = request.meta['proxy']
-        try:
-            del self.proxies[proxy]
-        except KeyError:
-            pass
+        err_proxy = request.meta['proxy']
+        for scheme in self.pool:
+            for midware in self.pool[scheme]:
+                creds, proxy = midware.proxies[scheme]
+                if err_proxy == proxy:
+                    self.pool[scheme].remove(midware)
 
-        log.info('Removing failed proxy <%s>, %d proxies left' % (
-                    proxy, len(self.proxies)))
+                    log.warning('Removing failed proxy <%s>, %d %s proxies left' % (
+                        proxy, len(self.pool[scheme]), scheme))
