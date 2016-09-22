56d9a2321a5e8b9e5397aab07d8be6262643846a
ThunderEX <thunderex@gmail.com>
2016-09-22 14:29:00 +0800

    improve behaviour during retry: allow alter proxy when retry for status code, remove obsolete proxy when retry

--- scrapy_proxies/randomproxy.py
+++ scrapy_proxies/randomproxy.py
@@ -1,8 +1,10 @@
 import os
 import random
 import logging
+from scrapy import Request
 from scrapy.exceptions import NotConfigured
 from scrapy.downloadermiddlewares.httpproxy import HttpProxyMiddleware
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
 from scrapy.utils.httpobj import urlparse_cached
 
 logger = logging.getLogger(__name__)
@@ -29,6 +31,9 @@ class RandomProxy(object):
         if not self.pool:
             raise NotConfigured
 
+        self.retry_midware = RetryMiddleware(crawler.settings)
+        self.discarded_proxies = []
+
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler)
@@ -42,11 +47,24 @@ class RandomProxy(object):
         except (IndexError, KeyError):
             raise ValueError('No available proxy, cannot proceed')
 
+        # retry request may have a obsolete proxy
+        if request.meta.get('proxy') in self.discarded_proxies:
+            del request.meta['proxy']
+        midware.process_request(request, spider)
         logger.debug('Using proxy <%s>, %d %s proxies left' % (
             midware.proxies[scheme], len(self.pool[scheme]), scheme))
-        midware.process_request(request, spider)
+
+    def process_response(self, request, response, spider):
+        # if retry middleware decides to retry, alter proxy
+        ret = self.retry_midware.process_response(request, response, spider)
+        if isinstance(ret, Request):
+            self.alter_proxy(request)
+        return response
 
     def process_exception(self, request, exception, spider):
+        self.alter_proxy(request)
+
+    def alter_proxy(self, request):
         if 'proxy' not in request.meta:
             return
 
@@ -56,6 +74,7 @@ class RandomProxy(object):
                 creds, proxy = midware.proxies[scheme]
                 if err_proxy == proxy:
                     self.pool[scheme].remove(midware)
+                    self.discarded_proxies.append(err_proxy)
 
                     logger.warning('Removing failed proxy <%s>, %d %s proxies left' % (
                         proxy, len(self.pool[scheme]), scheme))
